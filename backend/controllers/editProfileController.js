83a9c8068001df5d1085fb1e23e6cd20e013d053
Francis <francispulido68@gmail.com>
2025-10-19 17:39:54 +0800

    restructured routes and controllers and added profile editing functionality

---
+++ backend/controllers/editProfileController.js
@@ -0,0 +1,11 @@
+
+const UpdateuserProfile = async (req, res) => {
+    const { id, email, year, program, gender } = req.body;
+    try {
+        if (!id || !email || !year || !program || !gender) {
+            
+        }} catch (err) {}};
+        
+module.exports = {
+    UpdateuserProfile
+};
